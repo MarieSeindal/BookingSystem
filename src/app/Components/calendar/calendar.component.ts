824e9e21c8b4785b293890cd347e4317863d66e2
Marie Seindal <mse@Dalux.com>
2023-04-03 16:31:07 +0200

    mb

--- src/app/Components/calendar/calendar.component.ts
+++ src/app/Components/calendar/calendar.component.ts
@@ -11,6 +11,8 @@ import { CommonModule } from '@angular/common';
 })
 export class CalendarComponent implements OnInit{
 
+  public testNum = 2;
+
   // public dayInMonth = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
   public dayInMonth = this.makeMonthArray(31);
 
