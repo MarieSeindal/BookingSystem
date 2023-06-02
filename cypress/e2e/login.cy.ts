5e982430808947d6375e3c3729a3ad22dacccb18
Marie Seindal <mse@Dalux.com>
2023-06-02 13:30:14 +0200

    Cypress working

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,27 +1,27 @@
 /**
  * Test Login and out.
  * */
-// describe('Log in and out', () => {
-//   it('passes', () => {
-//     cy.visit('/login')
-//     cy.wait(500);
-//     cy.get('button').contains('Cypress').click()
-//     cy.get('button').contains('Log ud').click()
-//   })
-// })
-//
-// describe('Navigate to frontend documentation github', () => {
-//   it('passes', () => {
-//     cy.visit('/login')
-//     cy.wait(500);
-//     cy.get('button').contains('Cypress').click()
-//     cy.get('button').contains('Dokumentation').click()
-//     cy.get('a').contains('Frontend').click()
-//     cy.get('button').contains('Log ud').click()
-//   })
-// })
+describe('Log in and out', () => {
+  it('passes', () => {
+    cy.visit('/login')
+    cy.wait(500);
+    cy.get('button').contains('Cypress').click()
+    cy.get('button').contains('Log ud').click()
+  })
+})
 
-describe('Navigate to backend documentation github', () => {
+describe('Navigate to frontend documentation github', () => {
+  it('passes', () => {
+    cy.visit('/login')
+    cy.wait(500);
+    cy.get('button').contains('Cypress').click()
+    cy.get('button').contains('Dokumentation').click()
+    cy.get('a').contains('Frontend').click()
+    cy.get('button').contains('Log ud').click()
+  })
+})
+
+describe('Make a booking with all fields filled out', () => {
   it('passes', () => {
     cy.visit('/login')
     cy.wait(500);
@@ -32,8 +32,56 @@ describe('Navigate to backend documentation github', () => {
     cy.get("#cy_time").type("11:25")
     cy.get("#cy_duration").type("30")
     cy.get('mat-select[formControlName=room]').click().get('mat-option').contains('Rum 3').click();
-    // cy.get(mat-slide-toggle#cy_allDay .mat-slide-toggle-input').should('be.checked');
-    // cy.get('button').contains('Log ud').click()
+    cy.get("#cy_description").type("Cypress can help make automated testing!")
+    cy.get('button').contains('Gem bookning').click()
   })
 })
 
+describe('Make a booking with allDay', () => {
+  it('passes', () => {
+    cy.visit('/login')
+    cy.wait(500);
+    cy.get('button').contains('Cypress').click()
+    cy.get('button').contains('Booking').click()
+    cy.get("#cy_title").type("Title Cypress")
+    cy.get("#cy_calendarButton").click().get('button').contains("22").click()
+    cy.get('mat-select[formControlName=room]').click().get('mat-option').contains('Hal 1').click();
+    cy.get("#cy_allDay").click('left')
+    cy.get("#cy_description").type("Cypress can help make automated testing! it is very awesome :)")
+    cy.get('button').contains('Gem bookning').click()
+  })
+})
+
+
+describe('Delete booking', () => {
+  it('passes', () => {
+    cy.visit('/login')
+    cy.wait(500);
+    cy.get('button').contains('Cypress').click()
+    cy.get("#cy_delete").click()
+  })
+})
+
+describe('Edit booking', () => {
+  it('passes', () => {
+    cy.visit('/login')
+    cy.wait(500);
+    cy.get('button').contains('Cypress').click()
+    cy.get("#cy_edit").click()
+    cy.wait(500);
+    cy.get("#cy_title").type(" ").type("Edited Title Cypress")
+    cy.get("#cy_calendarButton").click().get('button').contains("14").click()
+    cy.get('mat-select[formControlName=room]').click().get('mat-option').contains('Hal 2').click();
+    cy.get("#cy_description").type(" ").type("Edited Cypress can help make automated testing! it is very awesome :)")
+    cy.get('button').contains('Opdater bookning').click()
+  })
+})
+
+describe('Delete last booking', () => {
+  it('passes', () => {
+    cy.visit('/login')
+    cy.wait(500);
+    cy.get('button').contains('Cypress').click()
+    cy.get("#cy_delete").click()
+  })
+})
